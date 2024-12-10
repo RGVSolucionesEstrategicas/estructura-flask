8dbf415528fbf3fc8ad405d75f405538f07275c1
christiangonzalezrgv <christian.gonzalez@rgvsoluciones.com>
2024-12-10 17:28:38 -0600

    v21

--- static/js/datatable_archivos.js
+++ static/js/datatable_archivos.js
@@ -1,32 +1,32 @@
 window.dataTable = function () {
 	return {
-			items: [],
-			fetchData() {
-					fetch("/files/data")
-							.then((response) => response.json())
-							.then((data) => {
-									if (data.error) {
-											console.error("Error:", data.error);
-									} else {
-											this.items = data.sort(this.compareOnKey("filename", "asc"));
-									}
-							})
-							.catch((error) => {
-									console.error("Error al obtener los datos:", error);
-							});
-			},
-			compareOnKey(key, rule) {
-					return function (a, b) {
-							let comparison = 0;
-							const fieldA = a[key].toLowerCase();
-							const fieldB = b[key].toLowerCase();
-							if (rule === "asc") {
-									comparison = fieldA > fieldB ? 1 : fieldA < fieldB ? -1 : 0;
-							} else {
-									comparison = fieldA < fieldB ? 1 : fieldA > fieldB ? -1 : 0;
-							}
-							return comparison;
-					};
-			},
+		items: [],
+		fetchData() {
+			fetch("/files/data")
+				.then((response) => response.json())
+				.then((data) => {
+					if (data.error) {
+						console.error("Error:", data.error);
+					} else {
+						this.items = data.sort(this.compareOnKey("filename", "asc"));
+					}
+				})
+				.catch((error) => {
+					console.error("Error al obtener los datos:", error);
+				});
+		},
+		compareOnKey(key, rule) {
+			return function (a, b) {
+				let comparison = 0;
+				const fieldA = a[key].toLowerCase();
+				const fieldB = b[key].toLowerCase();
+				if (rule === "asc") {
+					comparison = fieldA > fieldB ? 1 : fieldA < fieldB ? -1 : 0;
+				} else {
+					comparison = fieldA < fieldB ? 1 : fieldA > fieldB ? -1 : 0;
+				}
+				return comparison;
+			};
+		},
 	};
 };
